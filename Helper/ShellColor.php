2d4fad5e197b2e8c3f1f431b40f263b1542152e1
Yann Le Scouarnec <Ikimasoft@users.noreply.github.com>
2016-04-23 22:23:52 +0200

    JQuery + Unit tests (#20)
    * adding some unit tests
    
    * unit tests for Helper\TableBuilder
    
    * added credit for colors array in Helper\ShellColor

--- Helper/ShellColor.php
+++ Helper/ShellColor.php
@@ -3,6 +3,8 @@ namespace Helper;
 
 /**
  * Class ShellColor
+ * Thanks you JR from www.if-not-true-then-false.com
+ * He made a nice little article on coloring and I used his color array
  * @package Helper
  * @author Yann Le Scouarnec <yann.le-scouarnec@hetic.net>
  */
---
+++ Tests/TableBuilderTest.php
@@ -0,0 +1,97 @@
+<?php
+namespace Tests;
+
+use Helper\TableBuilder;
+
+/**
+ * Class TableBuilderTest
+ * @package Tests
+ * @author Yann Le Scouarnec <yann.le-scouarnec@hetic.net>
+ */
+class TableBuilderTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * provides data for testInit
+     */
+    public function tableBuilderProvider()
+    {
+        return [
+            [
+                [
+                    [
+                        'a long string of weird text',
+                        'str',
+                        '',
+                    ],
+                ],
+                null,
+                '+----------------------------+----+-+'.PHP_EOL.
+                '|a long string of weird text |str | |'.PHP_EOL.
+                '+----------------------------+----+-+'.PHP_EOL
+                ,
+            ],
+            [
+                [
+                    [
+                        '1',
+                        '2',
+                        '3',
+                    ],
+                    [
+                        '4',
+                        '5',
+                        '6',
+                    ],
+                ],
+                [
+                    'One',
+                    'Two',
+                    'Three',
+                ],
+                '+----+----+------+'.PHP_EOL.
+                '|One |Two |Three |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|1   |2   |3     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|4   |5   |6     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL
+                ,
+            ],
+        ];
+    }
+
+    /**
+     * @test Helper\TableBuilder::init
+     * @dataProvider tableBuilderProvider
+     * @param array $data
+     * @param null|array $headers
+     * @param string $expected
+     */
+    public function testInitMethod($data, $headers, $expected)
+    {
+        // test the TableBuilder
+        $output = TableBuilder::init($data, $headers);
+        $this->assertEquals($output, $expected);
+    }
+
+    /**
+     * @test Helper\TableBuilder::init
+     */
+    public function testInitRecordException()
+    {
+        // test the TableBuilder
+        $this->setExpectedException('\Exception', 'Oops, no data to build a table with :(');
+        TableBuilder::init(null, null);
+    }
+
+    /**
+     * @test Helper\TableBuilder::init
+     */
+    public function testInitDataException()
+    {
+        // test the TableBuilder
+        $this->setExpectedException('\Exception', 'Oops, no records to build a table with :(');
+        TableBuilder::init([null], null);
+    }
+}
