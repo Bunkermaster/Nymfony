529226c1d6ef95e5c5519dc9381c3ddbb1402cfc
Yann Le Scouarnec <Ikimasoft@users.noreply.github.com>
2016-06-03 13:03:08 +0200

    Fixed CLITableBuilder bugs, oops (#43)

--- Tests/CLITableBuilderTest.php
+++ Tests/CLITableBuilderTest.php
@@ -2,17 +2,21 @@
 namespace Tests;
 
 use Helper\CLITableBuilder;
+use phpunit\framework\TestCase;
 
 /**
  * Class CLITableBuilderTest
  * @package Tests
  * @author Yann Le Scouarnec <yann.le-scouarnec@hetic.net>
  */
-class CLITableBuilderTest extends \PHPUnit_Framework_TestCase
+class CLITableBuilderTest extends TestCase
 {
 
     /**
      * provides data for testInit
+     *  case #1 simple string table test
+     *  case #2 Simple table
+     *  case #3 Simple table with separator lines
      */
     public function tableBuilderProvider()
     {
@@ -26,6 +30,8 @@ class CLITableBuilderTest extends \PHPUnit_Framework_TestCase
                     ],
                 ],
                 null,
+                false,
+                false,
                 '+----------------------------+----+-+'.PHP_EOL.
                 '|a long string of weird text |str | |'.PHP_EOL.
                 '+----------------------------+----+-+'.PHP_EOL
@@ -49,14 +55,371 @@ class CLITableBuilderTest extends \PHPUnit_Framework_TestCase
                     'Two',
                     'Three',
                 ],
+                false,
+                false,
                 '+----+----+------+'.PHP_EOL.
                 '|One |Two |Three |'.PHP_EOL.
                 '+----+----+------+'.PHP_EOL.
                 '|1   |2   |3     |'.PHP_EOL.
-                '+----+----+------+'.PHP_EOL.
                 '|4   |5   |6     |'.PHP_EOL.
                 '+----+----+------+'.PHP_EOL
                 ,
+            ],
+            [
+                [
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                ],
+                [
+                    'One',
+                    'Two',
+                    'Three',
+                ],
+                true,
+                false,
+                '+----+----+------+'.PHP_EOL.
+                '|One |Two |Three |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL
+                ,
+            ],
+            [
+                [
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                ],
+                [
+                    'One',
+                    'Two',
+                    'Three',
+                ],
+                false,
+                5,
+                '+----+----+------+'.PHP_EOL.
+                '|One |Two |Three |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|One |Two |Three |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|One |Two |Three |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|One |Two |Three |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|One |Two |Three |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL
+            ],
+            [
+                [
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                    [
+                        'A',
+                        'B',
+                        'C',
+                    ],
+                    [
+                        'D',
+                        'E',
+                        'F',
+                    ],
+                ],
+                [
+                    'One',
+                    'Two',
+                    'Three',
+                ],
+                true,
+                false,
+                '+----+----+------+'.PHP_EOL.
+                '|One |Two |Three |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|A   |B   |C     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL.
+                '|D   |E   |F     |'.PHP_EOL.
+                '+----+----+------+'.PHP_EOL
             ],
         ];
     }
@@ -66,32 +429,34 @@ class CLITableBuilderTest extends \PHPUnit_Framework_TestCase
      * @dataProvider tableBuilderProvider
      * @param array $data
      * @param null|array $headers
+     * @param bool $separatorLines
+     * @param bool|int $repeatHeader
      * @param string $expected
      */
-    public function testInitMethod($data, $headers, $expected)
+    public function testInitMethod($data, $headers, $separatorLines, $repeatHeader, $expected)
     {
         // test the CLITableBuilder
-        $output = CLITableBuilder::init($data, $headers);
+        $output = CLITableBuilder::init($data, $headers, $separatorLines, $repeatHeader);
         $this->assertEquals($output, $expected);
     }
 
     /**
      * @test Helper\TableBuilder::init
+     * @expectedException \Exception
      */
     public function testInitRecordException()
     {
         // test the TableBuilder
-        $this->setExpectedException('\Exception', 'Oops, no data to build a table with :(');
         CLITableBuilder::init(null, null);
     }
 
     /**
      * @test Helper\TableBuilder::init
+     * @expectedException \Exception
      */
     public function testInitDataException()
     {
         // test the TableBuilder
-        $this->setExpectedException('\Exception', 'Oops, no records to build a table with :(');
         CLITableBuilder::init([null], null);
     }
 }
